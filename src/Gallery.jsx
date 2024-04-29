1f3dbc742486e05e025e05983866e80bce9ea4c1
sunny patel <sunnykpatel.skp@gmail.com>
2024-04-29 18:17:20 -0400

    add react query

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,4 +1,17 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
 const Gallery = () => {
+  const result = useQuery({
+    queryKey: ['galleryImages'],
+    queryFn: () =>
+      axios.get(
+        'https://api.unsplash.com/search/photos/?query=tiger&client_id=Oe7b-lrQ818X0izcd25pwf3dFrQ05Jk8tkCpruX4ESg'
+      ),
+  });
+
+  console.log(result);
+
   return <h2>Gallery</h2>;
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import AppContext from './AppContext.jsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <AppContext>
-    <App />
-  </AppContext>
+  <QueryClientProvider client={queryClient}>
+    <AppContext>
+      <App />
+    </AppContext>
+  </QueryClientProvider>
 );
